53e450045af42719b7c617c7ed2fdedbbaa3acaa
sonngok4 <ngoson2k40501@gmail.com>
2025-04-19 09:59:52 +0700

    merge son branch to main branch into son

--- my-app-client/src/routers/router.jsx
+++ my-app-client/src/routers/router.jsx
@@ -1,10 +1,7 @@
-import {
-  createBrowserRouter
-} from "react-router-dom";
-import App from '../App';
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
 import About from "../Components/About";
-import AuthForm from '../components/Auth/AuthForm';
-import ProtectedRoute from '../components/Auth/ProtectedRoute';
+import ProtectedRoute from "../components/Auth/ProtectedRoute";
 import Blog from "../Components/Blog";
 import Cart from "../Components/Cart";
 import Checkout from "../Components/Checkout";
@@ -20,7 +17,7 @@ import ManageBooks from "../dashboard/ManageBooks";
 import Profile from "../dashboard/Profile";
 import UploadBook from "../dashboard/UploadBook";
 import Home from "../homes/Home";
-import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import PrivateRoute from "../Components/Auth/PrivateRoute";
 import Shop from "../shops/Shop";
 import SingleBook from "../shops/SingleBook";
 
@@ -35,7 +32,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/shop",
-        element: <Shop />
+        element: <Shop />,
       },
       {
         path: "/about",
@@ -70,50 +67,43 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/admin/dashboard",
-        element: <PrivateRoute><Dashboard /></PrivateRoute>
+        element: (
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        ),
       },
       {
         path: "/admin/dashboard/upload",
-        element: <UploadBook />
-
+        element: <UploadBook />,
       },
       {
         path: "/admin/dashboard/manage",
-        element: <ManageBooks />
+        element: <ManageBooks />,
       },
       {
         path: "/admin/dashboard/edit-books/:id",
         element: <EditBooks />,
-        loader: ({ params }) => fetch(`http://localhost:3000/book/${params.id}`)
+        loader: ({ params }) =>
+          fetch(`http://localhost:3000/book/${params.id}`),
       },
       {
         path: "/admin/dashboard/profile",
-        element: <Profile />
+        element: <Profile />,
       },
       {
         path: "/admin/dashboard/details",
-        element: <Details />
-      }
-
-    ]
+        element: <Details />,
+      },
+    ],
   },
   {
     path: "sign-up",
-    element: <Signup />
-  }, {
+    element: <Signup />,
+  },
+  {
     path: "login",
-    element: <Login />
-  }, {
-    path: "logout",
-    element: <Logout />
-  },
-  {
-    path: '/login',
-    element: <AuthForm />,
-  },
-  {
-    path: '/register',
-    element: <AuthForm />,
+    element: <Login />,
   },
 ]);
 
